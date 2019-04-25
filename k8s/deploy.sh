02442df550a5066300e02f40e144e5f827f919bc
ryom0624 <ryo.m0624@gmail.com>
2019-04-25 16:04:55 +0900

    test

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -7,6 +7,7 @@ for f in k8s/sample/*.yml
 do
 	envsubst < $f > "generated-$(basename $f)"
 done
-gcloud docker -- push asia.gcr.io/${PROJECT_NAME}/sample:$CIRCLE_SHA1
+gcloud docker -- push asia.gcr.io/${PROJECT_NAME}/${CLOUD_REGISTRY_NAME}/web:$CIRCLE_SHA1
+gcloud docker -- push asia.gcr.io/${PROJECT_NAME}/${CLOUD_REGISTRY_NAME}/app:$CIRCLE_SHA1
 kubectl apply -f generated-deployment.yml --record
 kubectl apply -f generated-service.yml
