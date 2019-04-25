e37130e0eeba57d50b2375420aaa378a27fd2138
ryom0624 <ryo.m0624@gmail.com>
2019-04-25 17:23:07 +0900

    move container yaml

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -5,9 +5,9 @@ set -e
 
 for f in k8s/*.yaml
 do
-	envsubst < $f > "generated-$(basename $f)"
+	envsubst < $f > "k8s/generated-$(basename $f)"
 done
 gcloud docker -- push asia.gcr.io/${PROJECT_NAME}/${CLOUD_REGISTRY_NAME}/web:$CIRCLE_SHA1
 gcloud docker -- push asia.gcr.io/${PROJECT_NAME}/${CLOUD_REGISTRY_NAME}/app:$CIRCLE_SHA1
-kubectl apply -f generated-web.yml --record
-kubectl apply -f generated-db.yml --record
+kubectl apply -f k8s/generated-web.yml --record
+kubectl apply -f k8s/generated-db.yml --record
