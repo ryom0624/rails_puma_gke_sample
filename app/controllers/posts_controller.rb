66682dcef1e03a3b7fe7da5ed60e19df354b7a62
ryom0624 <ryo.m0624@gmail.com>
2019-05-13 15:25:59 +0900

    ver 0.2

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -69,6 +69,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title,:description,:user)
+      params.require(:post).permit(:title,:description,:user,:hoge)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_06_004024) do
+ActiveRecord::Schema.define(version: 2019_05_13_062321) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_05_06_004024) do
     t.datetime "updated_at", null: false
     t.text "description"
     t.text "user"
+    t.text "hoge"
   end
 
 end
